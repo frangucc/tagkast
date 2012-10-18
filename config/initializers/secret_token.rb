77be5721023f6d3399f422527eb30f9b07d756dc
Federico Bana <me@banafederico.com>
2012-10-18 02:56:33 -0200

    Initial commit 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gofoto::Application.config.secret_token = '9d7ec2c5195652c8857507b2517844a77f4348130185981a551402f37b4cc43893957f1e2d2a19939a7a4fcf0f4fae03f44649cd3d2f904efbdae209a27d78fc'
