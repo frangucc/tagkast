5f4e362abfd0c040742a3e282ccc8ced3103763f
Bhanu Prasad <bhanuprasad143@gmail.com>
2012-10-26 21:45:50 +0700

    send email from pricing page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -16,6 +16,19 @@ class SiteController < ApplicationController
   end
 
   def pricing
+    if request.post?
+      missing = []
+      missing << "Name" if params[:name].blank?
+      missing << "Email" if params[:email].blank?
+      missing << "Content" if params[:what].blank?
+      if missing.any?
+        flash.now[:alert] = missing.join(", ") << " cannot be empty."
+      else
+        ContactMailer.contact(params[:name], params[:email], params[:what]).deliver
+        flash.now[:notice] = "Email was sent to admin. Thank you for contact."
+      end
+    end
   end
 
 end
+
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,14 @@ Gofoto::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+ ActionMailer::Base.smtp_settings = {
+   :address        => "smtp.sendgrid.net",
+   :port           => "587",
+   :authentication => :plain,
+   :user_name      => 'prasadmadala',
+   :password       => '1234qwer',
+   :domain         => 'localhost:3000'
+ }
 end
+
